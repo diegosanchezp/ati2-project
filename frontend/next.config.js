b211cd47043fc8e71b95773e53d4477b8c567393
Diego Sánchez <diegosandmg@gmail.com>
2022-09-18 22:52:53 -0400

    refactor: swap mui for rsuitejs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,32 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(le|c)ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader'
+        },
+        {
+          loader: 'less-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    });
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'static/css/[name].css',
+        chunkFilename: 'static/css/[contenthash].css'
+      })
+    );
+
+    return config;
+  }
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,4 @@
 import type { NextPage } from 'next';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 import {djRequest} from "utils/apirest";
 import {withAuth, useSession} from "auth";
 
@@ -16,30 +10,10 @@ const Home: NextPage = () => {
   console.log(session);
 
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <button onClick={async ()=>{
-          await djRequest("logout");
-          dispatch({type: "setUser", user: null });
-        }}>logout</button>
-        <Copyright />
-      </Box>
-    </Container>
+    <button onClick={async ()=>{
+      await djRequest("logout");
+      dispatch({type: "setUser", user: null });
+    }}>logout</button>
   );
 };
 
