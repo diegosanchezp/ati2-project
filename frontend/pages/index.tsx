0c26d1af5c6f6aff0c6063a2769607d95c20321d
Diego Sánchez <diegosandmg@gmail.com>
2022-09-20 20:44:09 -0400

    refactor: delete not needed markup

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,11 @@
 import type { NextPage } from 'next';
-import {djRequest} from "utils/apirest";
 import {withAuth, useSession} from "auth";
 
 type HomePageProps = {};
 
 const Home: NextPage = () => {
-
-  const {session, dispatch} = useSession();
-  console.log(session);
-
   return (
-    <button onClick={async ()=>{
-      await djRequest("logout");
-      dispatch({type: "setUser", user: null });
-    }}>logout</button>
+    <></>
   );
 };
 
