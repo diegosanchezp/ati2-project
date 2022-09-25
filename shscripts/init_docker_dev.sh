fbd00ab4e237551a34314842686bd6e32d78d495
Diego Sánchez <diegosandmg@gmail.com>
2022-09-25 15:14:10 -0400

    feat: use gunicorn instead of django runserver

--- shscripts/init_docker_dev.sh
+++ shscripts/init_docker_dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#source .venv/bin/activate && gunicorn --log-level DEBUG --bind 0.0.0.0:8000 django_src.wsgi
+#source .venv/bin/activate && gunicorn --log-level DEBUG --reload --bind 0.0.0.0:8000 django_src.wsgi
 
 source .venv/bin/activate && \
 pushd ./backend && \
-python manage.py runserver 0.0.0.0:8000
+gunicorn --log-level DEBUG --reload --bind 0.0.0.0:8000 django_src.wsgi
+# python manage.py runserver 0.0.0.0:8000
