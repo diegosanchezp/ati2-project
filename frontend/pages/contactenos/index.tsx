697d044208b486b847ba1a6eebf1b9345ec307af
Diego Sánchez <diegosandmg@gmail.com>
2022-11-13 14:07:08 -0400

    feat: contactus page social media and i18n

--- frontend/pages/contactenos/index.tsx
+++ frontend/pages/contactenos/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import {withTranslations} from "utils/i18n";
 import {useTranslations} from 'next-intl';
-
+import {FaSvgIcon} from "components/FaSvgIcon";
 import { Modal, Button, ButtonToolbar, Grid, Row, Col, Form, Panel, FlexboxGrid, useToaster, Message } from 'rsuite';
 
 import type { NextPage } from 'next';
 import {Textarea} from "components/form";
 import { djRequest } from "utils/apirest";
+
+import { Icon } from '@rsuite/icons';
+import * as faTwitter from "@fortawesome/free-brands-svg-icons/faTwitter";
+import * as faFacebook from "@fortawesome/free-brands-svg-icons/faFacebook";
+import * as faInstagram from "@fortawesome/free-brands-svg-icons/faInstagram";
+import * as faLinkedin from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import * as faYoutube from "@fortawesome/free-brands-svg-icons/faYoutube";
 
 type InfoSectionProps = {
   title: string
@@ -29,10 +36,26 @@ function InfoSection(props: InfoSectionProps){
   );
 }
 
+type SocialMediaProps = {
+  name: string,
+  faIcon: any
+}
+
+function SocialMedia(props: SocialMediaProps){
+  const {name, faIcon} = props;
+  return (
+    <div style={{display: "flex", gap: "1em"}}>
+      <Icon as={FaSvgIcon} faIcon={faIcon} />
+      <p>{name}</p>
+    </div>
+  );
+}
+
 const ContactPage: NextPage = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const t = useTranslations('ContactUsPage');
   const toaster = useToaster();
 
   const toasterPlacement = {
@@ -66,33 +89,64 @@ const ContactPage: NextPage = () => {
     <Grid>
       <Row style={{margin: "1em"}}>
         <Col xs={12}>
-          <InfoSection title="Teléfonos">
+          <InfoSection title={t('telephone')}>
             <li>+58 (0212)-362-82-68</li>
             <li>+58+0414-389-74-44</li>
           </InfoSection>
 
-          <InfoSection title="Atención al público">
-            <li><b>Lunes a Viernes</b>: De 8 am a 12 m. Y de 1pm a 5pm</li>
-            <li><b>Sábados y Domingo</b>: De 8 am a 12 m. Y de 1pm a 5pm</li>
+          <InfoSection title={t('publicAttention')}>
+            <li><b>{t('mondayFriday.days')}</b>: {t('mondayFriday.hours')}</li>
+            <li><b>{t('satSunday.days')}</b>: {t('satSunday.hours')}</li>
           </InfoSection>
 
-          <InfoSection title="Correo electrónico">
-            <li>Envíanos tus preguntas o comentarios a 
-  nirvana01@gmail.com</li>
+          <InfoSection title={t('eMail')}>
+            <li>{t('eMailQuestion')}</li>
           </InfoSection>
 
           <InfoSection
-            title="Enlaces de interés"
+            title={t('linksOfInterest')}
             listStyleType="disc"
           >
-            <li><a onClick={handleOpen}>Preguntas Frecuentes</a></li>
-            <li><a>Términos de servicio</a></li>
+            <li><a onClick={handleOpen}>{t('faq')}</a></li>
+            <li><a>{t('terms')}</a></li>
           </InfoSection>
+          <FlexboxGrid>
+            <FlexboxGrid.Item colspan={12}>
+              <h3>{t('followUs')}</h3>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={12}>
+              <div style={{
+                display: "flex", flexDirection: "column",
+                gap: "0.5em"
+              }}>
+                <SocialMedia
+                  name="Facebook"
+                  faIcon={faFacebook}
+                />
+                <SocialMedia
+                  name="Twitter"
+                  faIcon={faTwitter}
+                />
+                <SocialMedia
+                  name="Youtube"
+                  faIcon={faYoutube}
+                />
+                <SocialMedia
+                  name="Instagram"
+                  faIcon={faInstagram}
+                />
+                <SocialMedia
+                  name="Linkedin"
+                  faIcon={faLinkedin}
+                />
+              </div>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
         </Col>
 
         <Col xs={12}>
           <ButtonToolbar>
-            <Button onClick={handleOpen}>Preguntas frecuentes</Button>
+            <Button onClick={handleOpen}>{t('faq')}</Button>
           </ButtonToolbar>
 
           <Modal open={open} onClose={handleClose}>
@@ -117,7 +171,7 @@ const ContactPage: NextPage = () => {
             </Modal.Footer>
           </Modal>
           <Panel
-            header={<h3>Formulario de contacto</h3>}
+            header={<h3>{t('contactForm.header')}</h3>}
             bordered
             style={{
               marginTop: "1em"
@@ -125,21 +179,21 @@ const ContactPage: NextPage = () => {
           >
             <Form fluid onSubmit={sendContactEmail} >
               <Form.Group controlId="to_mail">
-                <Form.ControlLabel>Para</Form.ControlLabel>
+                <Form.ControlLabel>{t('contactForm.for')}</Form.ControlLabel>
                 <Form.Control type="email" name="to_mail" required />
               </Form.Group>
               <Form.Group controlId="firstname_lastname">
-                <Form.ControlLabel>Nombre y apellido</Form.ControlLabel>
+                <Form.ControlLabel>{t('contactForm.nameLastname')}</Form.ControlLabel>
                 <Form.Control type="text" name="firstname_lastname" required />
               </Form.Group>
               <Form.Group controlId="body">
-                <Form.ControlLabel>Asunto</Form.ControlLabel>
+                <Form.ControlLabel>{t('contactForm.subject')}</Form.ControlLabel>
                 <Form.Control rows={5} accepter={Textarea} type="text" name="message" required />
               </Form.Group>
               <Form.Group>
                 <FlexboxGrid justify="center">
 
-                  <Button appearance="primary" type="submit">Enviar</Button>
+                  <Button appearance="primary" type="submit">{t('contactForm.send')}</Button>
                 </FlexboxGrid>
               </Form.Group>
             </Form>
