7c4e194089aa9505608632c452c8ac6a89b4dfa9
Osvani <osvacolijim@gmail.com>
2022-11-28 11:20:40 -0400

    Incluir datos de owner con su  serializador en lista de vehículos

--- backend/django_src/apps/api/serializers.py
+++ backend/django_src/apps/api/serializers.py
@@ -3,6 +3,24 @@ from ..vehicle.models import Vehicle, VehicleImages, VehicleVideos, VehicleModel
 from ..finance.serializers import CurrencySerializer
 from ..country.serializers import CitySerializer
 
+from django.contrib.auth import get_user_model
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        exclude = [
+            "password",
+            "last_login",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "user_type",
+            "language",
+            "keep_informed",
+            "groups",
+            "user_permissions"
+        ]
 
 class VehicleBrandSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,13 +53,12 @@ class VehicleSerializer(serializers.ModelSerializer):
     currency = CurrencySerializer(read_only = True)
     location_city = CitySerializer(read_only = True)
     model = VehicleModelSerializer(read_only = True)
+    owner = OwnerSerializer(read_only = True)
     images = VehicleImageSerializer(many = True, read_only = True)
     videos = VehicleVideosSerializer(many = True, read_only = True)
     class Meta: 
         model = Vehicle
-        exclude = [
-            "owner",
-        ]
+        exclude = []
 
 
 
