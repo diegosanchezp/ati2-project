494b26b9d54e78ee441020b8882b928898f72b00
Aarón Morillo <aaron@amobasoftware.com>
2022-12-08 00:25:19 -0400

    added edit page refactor

--- backend/django_src/apps/vehicle/urls.py
+++ backend/django_src/apps/vehicle/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('vehicle/brands', views.VehicleBrandView.as_view(), name='vehicle-brands'),
-    path('vehicle/models', views.VehicleModelView.as_view(), name='vehicle-models'),
+    path('vehicle/brand/<int:pk>/models', views.VehicleModelsByBrandView.as_view(), name='vehicle-models-by-brand'),
     path('vehicle', views.VehicleView.as_view(), name='vehicle-create'),
     # path('vehicle/<int:id>', views.VehicleView.as_view(), name='get-vehicle'),
     path('vehicle/<int:pk>', views.VehicleGetView.as_view(), name='get-vehicle'),
