98ccc24049240ed55a7561cabd13b142f1032c08
Aarón Morillo <aaron@amobasoftware.com>
2022-12-07 21:08:11 -0400

    added new form data requests

--- frontend/pages/api/vehicle/index.ts
+++ frontend/pages/api/vehicle/index.ts
@@ -1,5 +1,5 @@
 import { djRequest, getCSRF } from "utils/apirest";
-import {splitCookiesString} from "auth";
+import { splitCookiesString } from "auth";
 
 export const getVehiclesBrands = async () => {
   const response = await djRequest("vehicle/brands", {
@@ -13,61 +13,91 @@ export const getVehiclesBrands = async () => {
 };
 
 export const getVehiclesModels = async () => {
-    const response = await djRequest("vehicle/models", {
-      method: "GET",
-    });
-  
-    if (response.ok) {
-      const { models } = await response.json();
-      return models;
-    }
+  const response = await djRequest("vehicle/models", {
+    method: "GET",
+  });
+
+  if (response.ok) {
+    const { models } = await response.json();
+    return models;
+  }
+};
+
+export const createVehicle = async (_vehicleData: any, _isJSONData = true) => {
+  const { csrfToken, csrfRes } = await getCSRF();
+
+  let headers = {
+    "Content-Type": "",
+    "X-CSRFToken": csrfToken as string,
   };
 
-export const createVehicle = async (_vehicleData: any) => {
-  const { csrfToken, csrfRes } = await getCSRF();
+  if (_isJSONData)
+    headers = {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrfToken as string,
+    };
 
   const response = await djRequest("vehicle", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": csrfToken as string,
-    },
-    body: JSON.stringify({ vehicle: _vehicleData})    
+    headers: headers,
+    body: _isJSONData ? JSON.stringify(_vehicleData) : _vehicleData,
   });
 
   if (response.ok) {
     const { success } = await response.json();
-    console.log(success)
     return success;
+  } else {
+    return { success: false };
   }
-}
+};
 
 export const getVehicle = async (_id: string) => {
   const response = await djRequest(`vehicle/${_id}`, {
-    method: "GET",    
+    method: "GET",
   });
 
   if (response.ok) {
     const vehicleData = await response.json();
     return vehicleData;
   }
-}
+};
 
-export const editVehicle = async (_vehicleData: any, _id: string) => {
+export const editVehicle = async (_vehicleData: any, _id: string, _isJSONData = true) => {
   const { csrfToken, csrfRes } = await getCSRF();
 
-  const response = await djRequest(`vehicle/${_id}`, {
-    method: "PUT",
-    headers: {
+  let headers = {
+    "Content-Type": "",
+    "X-CSRFToken": csrfToken as string,
+  };
+
+  if (_isJSONData)
+    headers = {
       "Content-Type": "application/json",
       "X-CSRFToken": csrfToken as string,
-    },
-    body: JSON.stringify({ vehicle: _vehicleData})    
+    };
+
+  const response = await djRequest(`vehicle/edit/${_id}`, {
+    method: "POST",
+    credentials: "include",
+    headers: headers,
+    body: _isJSONData ? JSON.stringify(_vehicleData) : _vehicleData,
   });
 
   if (response.ok) {
     const { success } = await response.json();
-    console.log(success)
     return success;
+  } else {
+    return { success: false };
   }
-}
+};
+
+/*export const getModels = async (_brandId: number) => {
+  const response = await djRequest(`country/${}/states`, {
+    method: "GET",
+  });
+
+  if (response.ok) {
+    const { states } = await response.json();
+    return states;
+  }
+};*/
---
+++ frontend/utils/vehicle-lists.tsx
@@ -0,0 +1,110 @@
+export const continents = [
+  {
+    label: "Americas",
+    value: "Americas",
+  },
+  {
+    label: "Asia",
+    value: "Asia",
+  },
+  {
+    label: "Oceania",
+    value: "Oceania",
+  },
+  {
+    label: "Europe",
+    value: "Europe",
+  },
+  {
+    label: "Africa",
+    value: "Africa",
+  },
+];
+
+export const timeSystem = [
+  {
+    label: "AM",
+    value: "am",
+  },
+  {
+    label: "PM",
+    value: "pm",
+  },
+];
+
+export const hours = [
+  {
+    label: "01",
+    value: "01",
+  },
+  {
+    label: "02",
+    value: "02",
+  },
+  {
+    label: "03",
+    value: "03",
+  },
+  {
+    label: "04",
+    value: "04",
+  },
+  {
+    label: "05",
+    value: "05",
+  },
+  {
+    label: "06",
+    value: "06",
+  },
+  {
+    label: "07",
+    value: "07",
+  },
+  {
+    label: "08",
+    value: "08",
+  },
+  {
+    label: "09",
+    value: "09",
+  },
+  {
+    label: "10",
+    value: "10",
+  },
+  {
+    label: "11",
+    value: "11",
+  },
+  {
+    label: "12",
+    value: "12",
+  },
+];
+
+export const vehicleStatus = [
+  {
+    label: "New",
+    value: "NEW",
+  },
+  {
+    label: "Used",
+    value: "USED",
+  },
+];
+
+export const vehicleTypes = [
+  {
+    label: "truck",
+    value: "TRUCK",
+  },
+  {
+    label: "Van",
+    value: "VAN",
+  },
+  {
+    label: "Car",
+    value: "CAR",
+  },
+];
